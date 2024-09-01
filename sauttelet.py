f2d4f087fb8db1b65138b9d08fad8a5aee081ef6
Sandy Auttelet <107436258+sandyauttelet@users.noreply.github.com>
2024-08-31 17:29:14 -0700

    Update sauttelet.py

--- sauttelet.py
+++ sauttelet.py
@@ -64,11 +64,11 @@ with st.container():
     st.write("##")
     contact_form = """
     <form action="https://formsubmit.co/sandra.zichterman@wsu.edu" method="POST">
-    <input type="hidden" name="_captcha" value="false">
-         <input type="text" name="name" placeholder="Your name" required>
-         <input type="email" name="email" placeholder="Your email" required>
-         <textarea name="message" placeholder="Your message here" required></textarea>
-         <button type="submit">Send</button>
+        <input type="hidden" name="_captcha" value="false">
+        <input type="text" name="name" placeholder="Your name" required>
+        <input type="email" name="email" placeholder="Your email" required>
+        <textarea name="message" placeholder="Your message here" required></textarea>
+        <button type="submit">Send</button>
     </form>
     """
     left_column, right_column = st.columns(2)
