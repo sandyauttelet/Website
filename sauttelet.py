0be5dec80315bf675537d80341c5b955073d2e0f
Sandy Auttelet <107436258+sandyauttelet@users.noreply.github.com>
2024-09-01 20:48:50 -0700

    Update sauttelet.py

--- sauttelet.py
+++ sauttelet.py
@@ -37,7 +37,7 @@ def main_page():
 def page2():
     st.markdown("# CV")
     st.sidebar.markdown("# CV")
-    with open("pdfs/CV.pdf,"rb") as f:
+    with open("pdfs/CV.pdf, "r") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
         pdf_display = F'<embed src=”data:application/pdf;base64,{base64_pdf}” width=”700″ height=”1000″ type=”application/pdf”>’
     st.markdown(pdf_display, unsafe_allow_html=True)
